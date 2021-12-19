9e4e686dde952544846c26d4ddcaf3ab13569de9
Ehab <ealshobake.c@stc.com.sa>
2021-12-19 06:47:03 +0300

    module-1 commit

--- src/main/java/com/stc/Main.java
+++ src/main/java/com/stc/Main.java
@@ -1,8 +1,10 @@
 package com.stc;
 
-
 import com.stc.test3.TestingService;
 
+/**
+ * Module-1 main class
+ */
 public class Main {
     public static void main(String[] args) {
         TestingService.print();
